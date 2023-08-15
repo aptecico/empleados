4807f403b1099813213e12849685d9682ecde847
German Pinchao <german.pinchao@gmail.com>
2023-08-15 18:59:41 -0500

    estilos

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Primer programa en php</title>
+    </head>
+    <body>
+        <h1>Hola como estas estimados estudiantes</h1>
+    </body>
+</html>
