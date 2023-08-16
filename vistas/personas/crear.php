b1cf5c88244e10c950da681fa45f574fec895df1
aptecico <german.pinchao@gmail.com>
2023-08-15 21:37:11 -0500

    Add files via upload

--- vistas/personas/crear.php
+++ vistas/personas/crear.php
@@ -11,6 +11,35 @@
            <h3> Crear nueva persona</h3>
         </div>
         
+        <fieldset>
+            <legend>Datos basicos</legend>
+            <form class="form-group">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="form-group">
+                                <label for="nombre" class="form-label">Nombre</label>
+                                <input class="form-contol" type="text" id="nombre" name="nombre">
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="form-group">
+                                <label for="apellido" class="form-label">Apellido</label>
+                                <input class="form-contol" id="apellido" name="apellido">
+                        </div>
+                    </div>
+
+                </div>
+                <div class="mb-3">
+            <label for="disabledSelect" class="form-label">Disabled select menu</label>
+            <select id="disabledSelect" class="form-select">
+                <option>Disabled select</option>
+                <option>Disabled select 22</option>
+                <option>Disabled select 2</option>
+            </select>
+    </div>
+
+            </form>
+        </fieldset>
 
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
