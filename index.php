a40ce1c3fb0e0d03c10df2d68ae61f3422c97293
aptecico <german.pinchao@gmail.com>
2023-08-15 19:02:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
         <title>Primer programa en php</title>
     </head>
     <body>
-        <h1>Hola como estas estimados estudiantes</h1>
+        <h1>Hola como estas estimados estudiantes, como estasn en este día</h1>
     </body>
 </html>
