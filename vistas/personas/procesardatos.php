843b6fc1838e904ffdfcd19556436cf8a9a198a7
aptecico <german.pinchao@gmail.com>
2023-08-22 20:50:29 -0500

    Add files via upload

--- vistas/personas/procesardatos.php
+++ vistas/personas/procesardatos.php
@@ -7,22 +7,37 @@ $requestData['apellido']=$_REQUEST['apellido'];
 $requestData['edad']=$_REQUEST['edad'];
 $requestData['genero']=$_REQUEST['genero'];
 $requestData['ciudad']=$_REQUEST['ciudad'];
-$requestData['accion']=$_REQUEST['btnEnviar'];
+$accion=$_REQUEST['btnEnviar'];
 
 //=============ACCIONES===============
     switch($accion){
         case 'Guardar':
-                store($request);
+            print_r($requestData);
+                store($requestData,$conn);
             break;
+
     }
 
 //====================================
 //función para guardar los datos en la base de datos
-function store($request){
+function store($requestData,$conn){
+    $nombre=$requestData['nombre'];
+    $apellido=$requestData['apellido'];
+    $edad=$requestData['edad'];
+    $genero=$requestData['genero'];
+    $ciudad=$requestData['ciudad'];
+   print_r($requestData);
     //crear el script para guardar en la base de datos
     $sql="INSERT INTO personas(nombre,apellido,edad,genero,ciudad)
-    VALUES ('$nombre','')
+    VALUES ('$nombre','$apellido','$edad','$genero','$ciudad');
     ";
+     echo $sql;
+
+    if(mysqli_query($conn,$sql)){
+        header('Location: ./index.php');
+    }else{
+        echo "Error ".$sql." <br>".mysqli_error($conn);
+    }
 
 }
 
